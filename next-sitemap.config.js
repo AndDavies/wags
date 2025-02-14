820e7cf3c148273d5d34662402f386491ce56aca
madvaies <m.andrew.davies@gmail.com>
2025-02-14 16:47:41 -0500

    Updated Blog Styling v29

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://wagsandwanders.com',
+    generateRobotsTxt: true, // This will automatically generate a robots.txt file
+    // Optional: Exclude specific paths if needed
+    // exclude: ['/admin', '/login'],
+    // Optional: Additional configuration options can be added here
+  }
+  
