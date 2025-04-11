190ce572a42662faa7bc624e182ddb394bcd1038
madvaies <m.andrew.davies@gmail.com>
2025-04-11 07:19:18 -0400

    10 April refined Create Trip v5

--- app/test-client/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { createClient } from '@/lib/supabase-client';
-
-export default function TestClientPage() {
-  const supabase = createClient();
-  console.log('Client Component Supabase:', supabase);
-  return <div>Check the console for the Supabase client.</div>;
-}
